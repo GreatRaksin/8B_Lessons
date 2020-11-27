9c509e47ace449b221d4f9d3c6b36c1d2977709b
demidraksin <greatraksin@icloud.com>
2020-11-27 11:41:52 +0300

    adding task3 solution with attempts

--- l2711/task3.py
+++ l2711/task3.py
@@ -1,5 +1,15 @@
 from random import randint  # randint - случайное число
 
-secret_num = randint(1, 10)
-guess_num = 0
+secret_num = randint(1, 10)  # загадываем число от 1 до 9
+guess_num = 0  # сюда будем записывать числа, которые угадывают
+attempts = 0
 
+while guess_num != secret_num and attempts < 5:
+    attempts += 1
+    guess_num = int(input('Введите число от 1 до 10, пока не угадаете: '))
+
+
+if guess_num == secret_num:
+    print('Угадал! Молодец!')
+else:
+    print(f'Я загадал число {secret_num}')
