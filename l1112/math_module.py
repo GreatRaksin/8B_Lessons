ec8547592ba3413aaffc962f3ba3626378c1d82d
demidraksin <greatraksin@icloud.com>
2020-12-11 12:25:39 +0300

    Adding lesson 11.12

---
+++ l1112/math_module.py
@@ -0,0 +1,10 @@
+from math import *
+
+n = 1.5
+n1 = -1.5
+
+print(floor(1.7))  # округление в пол - целое наименьшее
+print(ceil(1.0001))  # округление в потолок - целое наибольшее
+print(floor(n1), ceil(n1))
+
+print(sqrt(9))
